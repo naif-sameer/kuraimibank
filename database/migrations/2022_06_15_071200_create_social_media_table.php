5f8a13257ab130bec846e18ecffd76d697dbbef3
naif <nayef7138@gmail.com>
2022-06-16 12:23:17 +0300

    update: social media migration

--- database/migrations/2022_06_15_071200_create_social_media_table.php
+++ database/migrations/2022_06_15_071200_create_social_media_table.php
@@ -6,31 +6,31 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-        Schema::create('social_media', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('link');
-            $table->string('icon');
+  /**
+   * Run the migrations.
+   *
+   * @return void
+   */
+  public function up()
+  {
+    Schema::create('social_media', function (Blueprint $table) {
+      $table->id();
+      $table->string('name')->unique();
+      $table->string('link');
+      $table->string('icon');
 
-            $table->boolean('is_active')->default(1);
-            $table->timestamps();
-        });
-    }
+      $table->boolean('is_active')->default(1);
+      $table->timestamps();
+    });
+  }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::dropIfExists('social_media');
-    }
+  /**
+   * Reverse the migrations.
+   *
+   * @return void
+   */
+  public function down()
+  {
+    Schema::dropIfExists('social_media');
+  }
 };
