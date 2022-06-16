e928b60cd4bc7d3360830540ebe69f0eb6fb65ad
naif <nayef7138@gmail.com>
2022-06-16 22:02:24 +0300

    update: service-point migration file

--- database/migrations/2022_06_15_072245_create_service_points_table.php
+++ database/migrations/2022_06_15_072245_create_service_points_table.php
@@ -6,40 +6,40 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-        Schema::create('service_points', function (Blueprint $table) {
-            $table->id();
-            $table->json('name');
-            $table->json('address');
-            $table->string('phone');
-            $table->string('second_phone');
-            $table->json('working_hours');
+  /**
+   * Run the migrations.
+   *
+   * @return void
+   */
+  public function up()
+  {
+    Schema::create('service_points', function (Blueprint $table) {
+      $table->id();
+      $table->json('name');
+      $table->json('address');
+      $table->json('working_hours');
+      $table->string('phone');
+      $table->string('second_phone');
 
-            $table
-                ->foreignId('city_id')
-                ->references('id')
-                ->on('cities')
-                ->onDelete('cascade');
+      $table
+        ->foreignId('city_id')
+        ->references('id')
+        ->on('cities')
+        ->onDelete('cascade');
 
-            $table->boolean('is_active')->default(1);
+      $table->boolean('is_active')->default(1);
 
-            $table->timestamps();
-        });
-    }
+      $table->timestamps();
+    });
+  }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::dropIfExists('service_points');
-    }
+  /**
+   * Reverse the migrations.
+   *
+   * @return void
+   */
+  public function down()
+  {
+    Schema::dropIfExists('service_points');
+  }
 };
