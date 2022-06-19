8c86082cfe82e4bd0d0eada4a1860a469c079b02
naif <nayef7138@gmail.com>
2022-06-19 23:09:37 +0300

    add: components/form button, input and label

---
+++ src/components/form/Button.vue
@@ -0,0 +1,5 @@
+<template>
+  <button class="px-12 py-2 rounded bg-green-600 text-green-50">
+    <slot />
+  </button>
+</template>
---
+++ src/components/form/Input.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import { useAttrs } from 'vue';
+
+const randomInputID = Math.floor(Math.random() * 1e8 * Date.now());
+
+defineProps(['modelValue', 'label', 'type', 'required']);
+
+let emit = defineEmits(['update:modelValue']);
+const updateValue = (event: any) => {
+  emit('update:modelValue', event.target.value);
+};
+</script>
+
+<template>
+  <div>
+    <label class="text-gray-700" :for="`input-${randomInputID}-${label}`">
+      {{ label }}
+    </label>
+    <input
+      :value="modelValue"
+      @input="updateValue"
+      :id="`input-${randomInputID}-${label}`"
+      :type="type"
+      :placeholder="label"
+      required
+      class="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 focus:outline-none focus:ring"
+    />
+  </div>
+</template>
