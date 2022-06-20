c160e5c3e49f7c66e12df03e602cc4857766b544
naif <nayef7138@gmail.com>
2022-06-20 23:55:43 +0300

    update: Button

--- src/components/form/Button.vue
+++ src/components/form/Button.vue
@@ -1,5 +1,20 @@
+<script setup lang="ts">
+type ButtonType = {
+  size?: 'sm' | 'md';
+};
+
+defineProps<ButtonType>();
+</script>
+
 <template>
-  <button class="px-12 py-2 rounded bg-green-600 text-green-50">
+  <button
+    class="rounded bg-green-600 text-green-50"
+    :class="{
+      'py-2 px-12': size === undefined,
+      'py-1 px-6 text-sm': size === 'sm',
+      'py-2 px-8 text-sm': size === 'md',
+    }"
+  >
     <slot />
   </button>
 </template>
