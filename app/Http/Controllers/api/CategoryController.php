f1d2311c47f732e162ff430dcb1bc31ccc604578
naif <nayef7138@gmail.com>
2022-06-21 23:45:23 +0300

    update: Category RequestFile validation roles

--- app/Http/Controllers/api/CategoryController.php
+++ app/Http/Controllers/api/CategoryController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\CategoryRequest;
 use App\Models\Category;
 use Illuminate\Http\Request;
 
@@ -13,32 +14,30 @@ class CategoryController extends Controller
     return Category::all();
   }
 
-  public function getOne(Request $request)
+  public function getOne($id)
   {
-    return Category::where('id', $request->id)->first();
+    return Category::where('id', $id)->first();
   }
 
-  public function save(Request $request)
+  public function save(CategoryRequest $request)
   {
-    // TODO add file upload trait
     return Category::create([
       'name'  =>  $request->input('name'),
     ]);
   }
 
-  public function update(Request $request)
+  public function update(CategoryRequest $request, $id)
   {
-    // TODO add file  upload trait
-    $res = Category::where('id', $request->id)->update([
+    $res = Category::where('id', $id)->update([
       'name'  =>  $request->input('name'),
     ]);
 
     return $res ? ['message' => "Category data updated"] : ['error' => true];
   }
 
-  public function activeToggle(Request $request)
+  public function activeToggle(Request $request, $id)
   {
-    $res = Category::where('id', $request->id)->update(['is_active' => $request->is_active]);
+    $res = Category::where('id', $id)->update(['is_active' => $request->is_active]);
 
     return $res ? ['message' => "active toggle updated"] : ['error' => true];
   }
