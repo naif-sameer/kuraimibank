7ff75cebd5d57b32d523c0fc36c0f1a7339a63b7
naif <nayef7138@gmail.com>
2022-06-21 10:30:16 +0300

    add: views/exchange-rates

---
+++ src/views/exchange-rates/ExchangeRatesView.vue
@@ -0,0 +1,175 @@
+<script setup lang="ts">
+import { useI18n } from 'vue-i18n';
+import { onMounted, ref } from 'vue';
+
+import { useExchangeRatesStore } from '@/stores/exchange-rates';
+
+import SectionTitle from '@/components/SectionTitle.vue';
+import Modal from '@/components/Modal.vue';
+import Input from '@/components/form/Input.vue';
+import Button from '@/components/form/Button.vue';
+
+import Table from './Table.vue';
+
+// render data from store
+onMounted(async () => {
+  store.getExchangeRates();
+});
+
+const { t } = useI18n();
+let store = useExchangeRatesStore();
+
+const createModal = ref(false);
+const showCreateModal = () => {
+  createModal.value = true;
+};
+
+const addExchangeRates = () => {
+  store.addExchangeRates();
+
+  // close the modal
+  createModal.value = false;
+
+  // reset item store data
+  store.resetItem();
+};
+
+const editModal = ref(false);
+const showEditModal = (data: any) => {
+  editModal.value = true;
+
+  // update store item data
+  store.item.name.ar = data.name.ar;
+  store.item.name.en = data.name.en;
+  store.item.buy = data.buy;
+  store.item.sale = data.sale;
+  store.item.id = data.id;
+};
+
+const updateExchangeRates = () => {
+  store.updateExchangeRates();
+
+  // close the modal
+  editModal.value = false;
+};
+</script>
+
+<template>
+  <!-- title -->
+  <SectionTitle class="flex justify-between">
+    <span>
+      {{ t('exchange-rates') }}
+    </span>
+
+    <Button @click="showCreateModal" size="sm">{{ t('add') }}</Button>
+  </SectionTitle>
+
+  <!-- content -->
+  <div class="w-full overflow-hidden rounded-lg shadow-xs mb-8">
+    <!-- table content -->
+    <div class="w-full overflow-hidden rounded-lg shadow-xs">
+      <div class="w-full overflow-x-auto">
+        <Table :showEditModal="showEditModal" />
+      </div>
+    </div>
+
+    <!-- TODO pagination -->
+    <!------ table pagination  ------>
+    <!-- <TablePagination /> -->
+  </div>
+
+  <!------ create item modal ------>
+  <Teleport to="#app-modal">
+    <form class="space-y-4" @submit.prevent="addExchangeRates">
+      <Modal
+        :open="createModal"
+        :hide-modal="() => (createModal = false)"
+        :modal-title="t('exchange-rates')"
+      >
+        <template #body>
+          <!------ arabic name ------>
+          <Input
+            v-model="store.item.name.ar"
+            type="text"
+            :label="t('exchange_rates.currency-arabic-name')"
+          />
+
+          <!------ english name ------>
+          <Input
+            v-model="store.item.name.en"
+            type="text"
+            :label="t('exchange_rates.currency-english-name')"
+          />
+
+          <!------ sale ------>
+          <Input
+            v-model="store.item.sale"
+            type="number"
+            :label="t('exchange_rates.sale')"
+          />
+
+          <!------ buy ------>
+          <Input
+            v-model="store.item.buy"
+            type="number"
+            :label="t('exchange_rates.buy')"
+          />
+        </template>
+
+        <template #footer>
+          <Button type="submit">
+            {{ t('add') }}
+          </Button>
+        </template>
+      </Modal>
+    </form>
+  </Teleport>
+
+  <!------ edit modal ------>
+  <Teleport to="#app-modal">
+    <form class="space-y-4" @submit.prevent="updateExchangeRates">
+      <Modal
+        :open="editModal"
+        :hide-modal="() => (editModal = false)"
+        :modal-title="t('exchange-rates')"
+      >
+        <div>test</div>
+        <template #body>
+          <!------ arabic name ------>
+          <Input
+            v-model="store.item.name.ar"
+            type="text"
+            :label="t('exchange_rates.currency-arabic-name')"
+          />
+
+          <!------ english name ------>
+          <Input
+            v-model="store.item.name.en"
+            type="text"
+            :label="t('exchange_rates.currency-english-name')"
+          />
+
+          <!------ sale ------>
+          <Input
+            v-model="store.item.sale"
+            type="number"
+            :label="t('exchange_rates.sale')"
+          />
+
+          <!------ buy ------>
+          <Input
+            v-model="store.item.buy"
+            type="number"
+            :label="t('exchange_rates.buy')"
+          />
+        </template>
+
+        <template #footer>
+          <Button type="submit">
+            {{ t('update') }}
+          </Button>
+        </template>
+      </Modal>
+    </form>
+  </Teleport>
+</template>
---
+++ src/views/exchange-rates/Table.vue
@@ -0,0 +1,78 @@
+<script setup lang="ts">
+import { useI18n } from 'vue-i18n';
+
+import { dateFormat } from '@/helpers/dateFormat';
+import { useExchangeRatesStore } from '@/stores/exchange-rates';
+
+import Switch from '@/components/form/Switch.vue';
+import Badge from '@/components/Badge.vue';
+import TableTd from '@/components/table/TableTd.vue';
+
+let store = useExchangeRatesStore();
+
+defineProps<{
+  showEditModal: any;
+}>();
+
+const { t, locale } = useI18n();
+</script>
+
+<template>
+  <table class="w-full whitespace-no-wrap">
+    <!------ table head ------>
+    <thead
+      class="text-xs font-semibold tracking-wide text-left text-gray-500 uppercase border-b bg-gray-50"
+    >
+      <tr>
+        <TableTd> {{ t('exchange_rates.currency-name') }} </TableTd>
+        <TableTd>{{ t('exchange_rates.sale') }}</TableTd>
+        <TableTd>{{ t('exchange_rates.buy') }}</TableTd>
+        <TableTd>{{ t('status') }}</TableTd>
+        <TableTd>{{ t('exchange_rates.updated-date') }}</TableTd>
+        <TableTd>{{ t('action') }}</TableTd>
+      </tr>
+    </thead>
+
+    <!------ table body ------>
+    <tbody class="bg-white divide-y text-gray-700">
+      <tr v-for="item in store.items">
+        <TableTd>
+          <span class="font-semibold">
+            {{ locale === 'en' ? item.name.en : item.name.ar }}
+          </span>
+        </TableTd>
+
+        <TableTd>
+          <span> {{ item.sale }} </span>
+        </TableTd>
+
+        <TableTd>
+          <span>{{ item.buy }} </span>
+        </TableTd>
+
+        <TableTd>
+          <Badge v-if="item.is_active" type="primary"> {{ t('active') }}</Badge>
+          <Badge v-else type="danger"> {{ t('not-active') }}</Badge>
+        </TableTd>
+
+        <TableTd>
+          <span class="text-sm">{{ dateFormat(item.updated_at) }}</span>
+        </TableTd>
+
+        <TableTd class="gap-3 flex">
+          <!-- edit modal button -->
+          <i
+            @click="showEditModal(item)"
+            class="fa fa-edit cursor-pointer text-purple-600"
+          ></i>
+
+          <!-- active toggle button -->
+          <Switch
+            :is-active="item.is_active"
+            @click="store.activeToggle(item.id, item.is_active)"
+          />
+        </TableTd>
+      </tr>
+    </tbody>
+  </table>
+</template>
