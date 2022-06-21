6bc8418cb186dd189cd4a1f2cf50ff38b415ae13
naif <nayef7138@gmail.com>
2022-06-21 09:59:44 +0300

    update: components/SectionTitle add v-bind=$attrs

--- src/components/SectionTitle.vue
+++ src/components/SectionTitle.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="py-2 px-4 bg-white rounded-lg mb-6">
-    <h2 class="text-lg font-semibold text-gray-900">
+    <h2 class="text-lg font-semibold text-gray-900 w-full" v-bind="$attrs">
       <slot />
     </h2>
   </div>
