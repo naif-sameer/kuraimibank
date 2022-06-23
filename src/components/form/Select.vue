a1213a4c0c3bd61e67150eafe9a6f372e5ad3a8b
naif <nayef7138@gmail.com>
2022-06-23 09:25:20 +0300

    fix: Select | default option selected

--- src/components/form/Select.vue
+++ src/components/form/Select.vue
@@ -31,7 +31,7 @@ const updateValue = (event: any) => {
       @change="updateValue"
       class="block w-full px-4 py-2 text-gray-700 bg-white border border-gray-200 rounded-md focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 focus:outline-none focus:ring"
     >
-      <option disabled selected>
+      <option disabled :selected="modelValue === ''">
         {{ label }}
       </option>
       <template v-for="option in options" :key="option.id">
