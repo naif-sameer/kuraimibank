002eec43eededbe48e475322d939940cbe555474
naif <nayef7138@gmail.com>
2022-06-29 10:25:51 +0300

    add: components/Breadcrumbs

---
+++ vue-dashboard/src/components/Breadcrumbs.vue
@@ -0,0 +1,30 @@
+<script lang="ts" setup>
+import { RouterLink } from 'vue-router';
+
+type BreadcrumbsType = {
+  items: Array<{
+    text: string;
+    href: string;
+    disabled?: boolean;
+  }>;
+};
+
+defineProps<BreadcrumbsType>();
+</script>
+
+<template>
+  <div class="flex gap-2 items-center">
+    <template v-for="(item, index) in items">
+      <RouterLink v-if="!item.disabled" class="text-blue-500 hover:text-blue-600" :to="item.href">
+        {{ item.text }}
+      </RouterLink>
+
+      <span v-else> {{ item.text }} </span>
+
+      <!-- don't render it with the latest element -->
+      <template v-if="items.length > 1 && index !== items.length - 1">
+        <span class="text-gray-400"> / </span>
+      </template>
+    </template>
+  </div>
+</template>
