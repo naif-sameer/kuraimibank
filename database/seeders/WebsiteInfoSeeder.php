99b7b6bf4b13dc13a0a799a02a73ee1dc839c324
naif <nayef7138@gmail.com>
2022-07-19 13:36:02 +0300

    update: WebsiteInfoSeeder

--- database/seeders/WebsiteInfoSeeder.php
+++ database/seeders/WebsiteInfoSeeder.php
@@ -8,43 +8,48 @@ use Illuminate\Database\Seeder;
 
 class WebsiteInfoSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
-    public function run()
-    {
-        WebsiteInfo::insert([
-            [
-                'table_key'     => 'apply_for_service',
-                'table_value'     => '{"ar": "<p>apply_for_service</p>", "en": "<p>apply_for_service</p>"}',
-            ],
+  /**
+   * Run the database seeds.
+   *
+   * @return void
+   */
+  public function run()
+  {
+    WebsiteInfo::insert([
+      [
+        'table_key'     => 'apply_for_service',
+        'table_value'   => '{"ar": "<p>apply_for_service</p>", "en": "<p>apply_for_service</p>"}',
+        'created_at'    => now()
+      ],
+
+      [
+        'table_key'     => 'contact_us',
+        'table_value'   => '{"phone":123,"fax":234,"free_phone":123456,"mail_box":"bla bla","email":"email"}',
+        'created_at'    => now()
+      ],
+
+      [
+        'table_key'     => 'about_us',
+        'table_value'   => '{"ar": "<p>about_us</p>", "en": "<p>about_us</p>"}',
+        'created_at'    => now()
+      ],
+      [
+        'table_key'     => 'values_principles',
+        'table_value'   => '{"ar": "<p>values_principles</p>", "en": "<p>values_principles</p>"}',
+        'created_at'    => now()
+      ],
+      [
+        'table_key'     => 'strategy_statement',
+        'table_value'   => '{"ar": "<p>strategy_statement</p>", "en": "<p>strategy_statement</p>"}',
+        'created_at'    => now()
+      ],
+      [
+        'table_key'     => 'board_members',
+        'table_value'   => '{"ar": "<p>board_members</p>", "en": "<p>board_members</p>"}',
+        'created_at'    => now()
+      ],
 
 
-            [
-                'table_key'     => 'contact_us',
-                'table_value'     => '{"phone":123,"fax":234,"free_phone":123456,"mail_box":"bla bla","email":"email"}',
-            ],
-
-            [
-                'table_key'     => 'about_us',
-                'table_value'     => '{"ar": "<p>about_us</p>", "en": "<p>about_us</p>"}',
-            ],
-            [
-                'table_key'     => 'values_principles',
-                'table_value'     => '{"ar": "<p>values_principles</p>", "en": "<p>values_principles</p>"}',
-            ],
-            [
-                'table_key'     => 'strategy_statement',
-                'table_value'     => '{"ar": "<p>strategy_statement</p>", "en": "<p>strategy_statement</p>"}',
-            ],
-            [
-                'table_key'     => 'board_members',
-                'table_value'     => '{"ar": "<p>board_members</p>", "en": "<p>board_members</p>"}',
-            ],
-
-
-        ]);
-    }
+    ]);
+  }
 }
