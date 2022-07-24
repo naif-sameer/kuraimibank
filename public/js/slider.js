6aabf3097730a30ff8db18158ac8361808a1b1a4
naif <nayef7138@gmail.com>
2022-07-24 16:21:44 +0300

    update: slider.js
    - add availableMoveMode

--- public/js/slider.js
+++ public/js/slider.js
@@ -9,19 +9,22 @@ const el = ($el) => document.querySelector($el);
 const els = ($els) => document.querySelectorAll($els);
 
 function superSlide({
-  slide,
+  slider,
   backBtn,
   forwardBtn,
   paginationItems,
   activeClass = "active-slide",
+  onSlideChange = (targetIndex, slideItem) => {},
+
+  availableMoveMode = false,
 }) {
-  const slideItems = els(`${slide} > *`);
+  const slideItems = els(`${slider} > *`);
 
   const getDirection = () =>
-    window.getComputedStyle(el(slide)).getPropertyValue("direction");
+    window.getComputedStyle(el(slider)).getPropertyValue("direction");
 
-  const getSliderWidth = (item) =>
-    window.getComputedStyle(item).getPropertyValue("width");
+  const getItemWidth = (item) =>
+    parseFloat(window.getComputedStyle(item).getPropertyValue("width"));
 
   let activeSlide = 0;
   let totalSlides = slideItems.length; // slide items count
@@ -34,8 +37,30 @@ function superSlide({
   if (forwardBtn)
     el(forwardBtn).addEventListener("click", () => changeSlideItem("forward"));
 
+  let baseAvailableMove =
+    totalSlides > 0
+      ? totalSlides -
+        Math.round(getItemWidth(el(slider)) / getItemWidth(slideItems[0])) +
+        1
+      : 0;
+
+  let availableMove = baseAvailableMove;
+
+  function checkAvailableSlideMove(direction) {
+    if (availableMove < baseAvailableMove && direction === "back")
+      availableMove++;
+
+    if (availableMove > 0 && direction === "forward") availableMove--;
+  }
+
   function changeSlideItem(direction) {
     let targetSlide = direction === "back" ? activeSlide - 1 : activeSlide + 1;
+
+    if (availableMoveMode) {
+      checkAvailableSlideMove(direction);
+
+      if (availableMove <= 0) return;
+    }
 
     if (targetSlide < 0) animateSlideItem(0);
     else if (targetSlide >= totalSlides) animateSlideItem(totalSlides - 1);
@@ -49,17 +74,21 @@ function superSlide({
     });
   }
 
-  function animateSlideItem(target) {
+  function animateSlideItem(target, direction = null) {
+    // call onSlideChange
+    onSlideChange(target, slideItems[target]);
+
     if (paginationItems) changeActivePaginationItem(target);
 
-    let slideWidth = parseFloat(getSliderWidth(slideItems[target])) * target;
+    let slideMoveWidth = getItemWidth(slideItems[target]) * target;
+
     let slideDirection = getDirection() === "ltr" ? "-" : "";
 
     makeActiveSlide(target);
 
     el(
-      slide
-    ).style.transform = `translate3d(${slideDirection}${slideWidth}px, 0px, 0px)`;
+      slider
+    ).style.transform = `translate3d(${slideDirection}${slideMoveWidth}px, 0px, 0px)`;
 
     activeSlide = target;
   }
