8010284fe2e1c4e2dd06ec1cdede2ba11b2fdb6f
naif <nayef7138@gmail.com>
2022-07-24 19:38:03 +0300

    update: service view
    - hide empty sections

--- resources/views/components/web/service/service-quote.blade.php
+++ resources/views/components/web/service/service-quote.blade.php
@@ -1,7 +1,11 @@
-<section class="service-quote">
-  <div class="container">
-    <p class="description">
-      {!! $service->service_slogan !!}
-    </p>
-  </div>
-</section>
+@isset($service->service_slogan)
+  @if ($service->service_slogan)
+    <section class="service-quote">
+      <div class="container">
+        <p class="description">
+          {!! $service->service_slogan !!}
+        </p>
+      </div>
+    </section>
+  @endif
+@endisset
